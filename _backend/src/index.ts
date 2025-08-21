923e6098fd36aefe355719d3b29fd75b8b51255c
0xLS <zerodna1@gmail.com>
2025-08-21 09:34:40 +0200

    template data endpoints

--- _backend/src/index.ts
+++ _backend/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
+
 import userRoutes from './routes/user';
+import dataRoutes from './routes/data';
 
 const app = express();
 const port = 3000;
@@ -12,6 +14,7 @@ app.use(cors({
 }))
 
 app.use('/user', userRoutes);
+app.use('/data', dataRoutes);
 
 app.get('/', (req, res) => {
 	res.send('meow');
