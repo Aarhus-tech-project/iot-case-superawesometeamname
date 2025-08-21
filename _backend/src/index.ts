545dc1a477f3477e8190dae93d3643dc51a031d4
0xLS <zerodna1@gmail.com>
2025-08-21 09:23:22 +0200

    backend

--- _backend/src/index.ts
+++ _backend/src/index.ts
@@ -1,20 +1,12 @@
 import express from 'express';
-import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
-app.use(express.json())
-app.use(cors({
-	origin: '*', // or specify origin
-	methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}));
-
-// Routes
-app.get('/users', (req, res) => {
-	res.json({ users: ['Alice', 'Bob'] });
+app.get('/', (req, res) => {
+	res.send('Hello from Express + TypeScript!');
 });
 
 app.listen(port, () => {
-	console.log(`🚀 Server running at http://localhost:${port}`);
+	console.log(`🚀 Server running on http://localhost:${port}`);
 });
