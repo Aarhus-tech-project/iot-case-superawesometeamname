7bdcf4cb52559b89ac2eddf01ef4da635001780d
0xLS <zerodna1@gmail.com>
2025-09-02 10:27:40 +0200

    fix

--- _mqtt/src/processors/RequestHandler.ts
+++ _mqtt/src/processors/RequestHandler.ts
@@ -8,26 +8,37 @@ type IPublishPacket = PublishPacket | PublishPacket & {
 
 class RequestHandler {
 	receivedPubMessage = async (client: Client, packet: IPublishPacket) => {
-		console.log(`Received message from ${client.id} on topic ${packet.topic}`)
+		console.log(`RequestHandler::receivedPubMessage - Received message from ${client.id} on topic ${packet.topic}`)
 
 		const { payload } = packet;
 
 		if (typeof payload !== 'string') {
-			console.log(`Message from ${client.id} is invalid`)
+			console.log(`RequestHandler::receivedPubMessage - Message from ${client.id} is invalid`)
 			return;
 		}
 
+		const decoded: {
+			userId: number;
+			steps: number;
+			distance: number;
+			gforce: number;
+			spo2: number;
+			bpm: number;
+			temp: number;
+		} = JSON.parse(payload);
+
+		console.log("RequestHandler::receivedPubMessage - decoded json body:", decoded)
+
 		// userId,heartRate,steps,distance,gforce
-		const split = payload.split(',').map(value => parseFloat(value));
+		/*const split = payload.split(',').map(value => parseFloat(value));
 
 		const userId = split[0];
 		const heartrate = split[1];
 		const steps = split[2];
 		const distance = split[3];
-		const gforce = split[4];
+		const gforce = split[4];*/
 
-		db.insertData(userId, heartrate, steps, distance, gforce);
-
+		//db.insertData(userId, heartrate, steps, distance, gforce);
 	}
 
 	empty = () => console.log("\n")
